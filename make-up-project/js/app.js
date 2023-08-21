ca411cfbb0e401315bd495ee5da311cfa31c13b0
Lois Lene <loislene.of.b@gmail.com>
2023-08-21 01:02:07 -0400

    task: render products data (not working)

--- make-up-project/js/app.js
+++ make-up-project/js/app.js
@@ -1,3 +1,79 @@
+let products = [];
+let brands = [];
+let types = [];
+
+const listEl = document.querySelector("ul");
+const formEl = document.querySelector("form");
+const inputfilterName = document.getElementById("filter-name");
+const selectFilterBrands = document.getElementById("filter-brand");
+const selectFilterTypes = document.getElementById("filter-type");
+const selectSortType = document.getElementById("sort-type");
+
+async function init(){
+  try {  
+    products = await listProducts(),listBrandOrType();
+    renderFormData();
+    renderData();
+    clearSelection();
+  } catch (erro) {
+    showError(erro);
+  }
+}
+init();
+
+function selectItem(product, li) {
+  clearSelection();
+  selectedItem = product;
+  li.classList.add("selected");
+}
+
+function clearSelection() {
+  clearError();
+  selectedItem = undefined;
+  const li = listEl.querySelector(".selected");
+  if (li) {
+    li.classList.remove("selected");
+  }
+}
+
+function renderFormData() {
+  console.log(brands);
+  for (const brand of brands) {
+    const brandOption = document.createElement("option");
+    brandOption.value = brand;
+    brandOption.textContent = brand;
+    selectFilterBrands.appendChild(op);
+  }
+  for (const type of types) {
+    const typeOption = document.createElement("option");
+    typeOption.value = type;
+    typeOption.textContent = type;
+    selectFilterTypes.appendChild(typeOption);
+  }
+}
+
+function renderData() {
+  console.log(products);
+  listEl.innerHTML = "";
+  for (const product of products) {
+    const li = document.createElement("li");
+    const divName = document.createElement("div");
+    divName.textContent = product.name;
+    const divBrand = document.createElement("div");
+    divBrand.textContent = product.brand;
+    li.appendChild(divName);
+    li.appendChild(divBrand);
+    listEl.appendChild(li);
+  }
+}
+
+function showError(error){
+  document.getElementById("errors").textContent = "Erro ao carregar dados.";
+  console.error(error);
+}
+function clearError() {
+  document.getElementById("errors").textContent = "";
+}
 //EXEMPLO DO CÓDIGO PARA UM PRODUTO
 function productItem(product) {
   const item = `<div class="product" data-name="NYX Mosaic Powder Blush Paradise" data-brand="nyx" data-type="bronzer" tabindex="508">
